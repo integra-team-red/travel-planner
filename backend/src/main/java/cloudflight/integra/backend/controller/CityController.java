3ca38a0bbe86e2731fd27d3c65e33a937051fcea
Goroshin <marius.info.bin@gmail.com>
2025-09-03 19:13:01 +0300

    chore(city): Added appropriate mapping to controller.

--- backend/src/main/java/cloudflight/integra/backend/controller/CityController.java
+++ backend/src/main/java/cloudflight/integra/backend/controller/CityController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController(value = "/city")
+@RestController()
+@RequestMapping("/city")
 public class CityController {
     private CityService service;
 
