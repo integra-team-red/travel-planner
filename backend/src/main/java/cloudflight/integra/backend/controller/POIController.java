955224014d9c6eea198c70757124c09875317630
CRLXT <corlatemanuel@gmail.com>
2025-09-05 01:21:31 +0300

    chore: Removed useless newline

--- backend/src/main/java/cloudflight/integra/backend/controller/POIController.java
+++ backend/src/main/java/cloudflight/integra/backend/controller/POIController.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController()
 @RequestMapping("/point-of-interest")
-
 public class POIController {
     private final POIService service;
 
