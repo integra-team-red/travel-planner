6a5034c6935d21d4cfef546c23542dce0c2955ae
Leo Mihalcea <leomihalcea@gmail.com>
2025-09-05 16:51:37 +0300

    chore(db): add hibernate and flyway
    - hibernate will be our orm
    - flyway will be our db migration system
    - wrote first migration, for city
    - added database data source for postgres connection
    
    Refs: #24

--- backend/src/main/java/cloudflight/integra/backend/model/City.java
+++ backend/src/main/java/cloudflight/integra/backend/model/City.java
@@ -1,7 +1,17 @@
 package cloudflight.integra.backend.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+
     String name;
 
     public int getId() {
