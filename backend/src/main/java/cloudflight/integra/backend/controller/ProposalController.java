0b77e0cfc3f441c921e28073c159369b103da152
bonti23 <alexandramiha04@gmail.com>
2025-09-10 18:02:52 +0300

    feat(Proposal): migration into the database
    Refs: #40

--- backend/src/main/java/cloudflight/integra/backend/controller/ProposalController.java
+++ backend/src/main/java/cloudflight/integra/backend/controller/ProposalController.java
@@ -3,7 +3,7 @@ package cloudflight.integra.backend.controller;
 import cloudflight.integra.backend.DTO.ProposalDTO;
 import cloudflight.integra.backend.mapper.ProposalMapper;
 import cloudflight.integra.backend.model.Proposal;
-import cloudflight.integra.backend.service.ServiceProposal;
+import cloudflight.integra.backend.service.ProposalServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,22 +15,22 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/proposal")
 public class ProposalController {
-    private ServiceProposal serviceProposal;
+    private ProposalServiceImpl proposalService;
 
     @Autowired
-    private void setProposalService(ServiceProposal serviceProposal) {
-        this.serviceProposal = serviceProposal;
+    private void setProposalService(ProposalServiceImpl serviceProposal) {
+        this.proposalService = serviceProposal;
     }
 
     @PostMapping
-    public ResponseEntity<ProposalDTO> createProposal(@RequestBody ProposalDTO proposalDTO) {
-        Proposal savedProposal = serviceProposal.save(ProposalMapper.ProposalToEntity(proposalDTO));
+    public ResponseEntity<ProposalDTO> createProposal(@RequestBody ProposalDTO proposalDTO){
+        Proposal savedProposal = proposalService.save(ProposalMapper.ProposalToEntity(proposalDTO));
         return ResponseEntity.ok(ProposalMapper.ProposalToDTO(savedProposal));
     }
 
     @GetMapping
     public ResponseEntity<List<ProposalDTO>> getAllProposals() {
-        List<ProposalDTO> proposals = serviceProposal.findAll()
+        List<ProposalDTO> proposals = proposalService.findAll()
                 .stream()
                 .map(ProposalMapper::ProposalToDTO)
                 .collect(Collectors.toList());
@@ -38,22 +38,24 @@ public class ProposalController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProposalDTO> getProposalById(@PathVariable Long id) {
-        Proposal proposal = serviceProposal.findById(id)
+    public ResponseEntity<ProposalDTO> getProposalById(@PathVariable("id") Long id) {
+        Proposal proposal = proposalService.findById(id)
                 .orElseThrow(() -> new RuntimeException("Proposal not found: " + id));
 
         return ResponseEntity.ok(ProposalMapper.ProposalToDTO(proposal));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProposalDTO> updateProposal(@PathVariable Long id, @RequestBody ProposalDTO newProposalDTO) {
-        Proposal updatedProposal = serviceProposal.update(ProposalMapper.ProposalToEntity(newProposalDTO));
+    public ResponseEntity<ProposalDTO> updateProposal(@PathVariable("id") Long id, @RequestBody ProposalDTO newProposalDTO) {
+        Proposal proposalToUpdate = ProposalMapper.ProposalToEntity(newProposalDTO);
+        proposalToUpdate.setId(id);
+        Proposal updatedProposal = proposalService.update(proposalToUpdate);
         return ResponseEntity.ok(ProposalMapper.ProposalToDTO(updatedProposal));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ProposalDTO> deleteProposal(@PathVariable Long id) {
-        Proposal deletedProposal = serviceProposal.deleteById(id);
+    public ResponseEntity<ProposalDTO> deleteProposal(@PathVariable("id") Long id) {
+        Proposal deletedProposal = proposalService.deleteById(id);
         return ResponseEntity.ok(ProposalMapper.ProposalToDTO(deletedProposal));
     }
 }
