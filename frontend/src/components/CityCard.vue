5adc1b09ab09821d88f25fe86e5ab0058850f3e2
CRLXT <corlatemanuel@gmail.com>
2025-10-01 23:02:52 +0300

    fix(city): implement update for City CRUD UI -fixed update function on backend -removed 'reactive' usage -replaced 'toggleInAddingMode' logic with simpler variable

--- frontend/src/components/CityCard.vue
+++ frontend/src/components/CityCard.vue
@@ -6,16 +6,18 @@
         selected: boolean
     }>()
 
-    const emits = defineEmits(["checkbox-clicked"])
+    const emits = defineEmits(["checkbox-clicked", "card-clicked"])
 </script>
 
 <template>
-    <Card class="w-full my-8">
+    <Card class="w-full my-8" @click="emits('card-clicked')">
         <template #title>{{city.name}}</template>
         <template #content>
             <div class="flex justify-between">
                 <p class="m-0">County, Country</p>
-                <Checkbox :value="selected" binary @click="emits('checkbox-clicked')"/>
+                <span @click.stop>
+                    <Checkbox :value="selected" binary @click="emits('checkbox-clicked')"/>
+                </span>
             </div>
         </template>
     </Card>
