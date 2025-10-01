c5158d201a52555ac3cac2064a64c39dcf492b28
Leo Mihalcea <leomihalcea@gmail.com>
2025-10-01 18:29:59 +0300

    chore(Navbar): implement navigation menu with highlight
    Refs: #74

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
     import type {MenuItem} from 'primevue/menuitem';
-    import {useRouter} from 'vue-router';
+    import {useRouter, useRoute} from 'vue-router';
     import LanguageSelectBox from '@/components/LanguageSelectBox.vue';
 
     const router = useRouter();
+    const route = useRoute();
 
-    const items: MenuItem[] = [
+    const items: (MenuItem & { path: string })[] = [
         {
             label: 'Home',
             icon: 'pi pi-link',
+            path: '/',
             command: () => {
                 router.push('/');
             }
@@ -16,13 +18,31 @@
         {
             label: 'Cities',
             icon: 'pi pi-building',
+            path: '/cities',
             command: () => {
                 router.push('/cities');
             }
         },
         {
-            label: 'Points of interest',
-            icon: 'pi pi-building',
+            label: 'Trips',
+            icon: 'pi pi-briefcase',
+            path: '/trips',
+            command: () => {
+                router.push('/trips')
+            }
+        },
+        {
+            label: 'Restaurants',
+            icon: 'pi pi-link',
+            path: '/restaurants',
+            command: () => {
+                router.push('/restaurants');
+            }
+        },
+        {
+            label: 'Points of Interest',
+            icon: 'pi pi-link',
+            path: '/points-of-interest',
             command: () => {
                 router.push('/points-of-interest');
             }
@@ -33,10 +53,10 @@
 <template>
     <Menubar :model="items" class="lg:max-w-6xl mx-auto mt-2">
         <template #start>
-            <!--todo: add logo here-->
+            <img src="/images/logo.png" alt="Logo" class="h-10 w-10" />
         </template>
         <template #item="{ item, props, hasSubmenu, root }">
-            <a v-ripple class="flex items-center" v-bind="props.action">
+            <a v-ripple class="flex items-center" v-bind="props.action" :class="{'bg-gray-400 text-white': item.path === '/' ? route.path === '/' : route.path.startsWith(item.path)}">
                 <span>{{item.label}}</span>
                 <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge"/>
                 <span v-if="item.shortcut"
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,6 +6,7 @@ import LoginView from '@/pages/LoginView.vue';
 import PointsOfInterestView from "@/pages/PointsOfInterestView.vue";
 import POIImportExport from "@/pages/POIImportExport.vue";
 import CitiesImportExport from "@/pages/CitiesImportExport.vue";
+import TripsView from '@/pages/TripsView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,6 +45,11 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: LoginView
+        },
+        {
+            path: '/trips',
+            name: 'trips',
+            component: TripsView
         }
     ],
 })
