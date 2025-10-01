1d6087601a555775626635c75961b72585c49aba
Goroshin <marius.info.bin@gmail.com>
2025-10-01 18:50:50 +0300

    feat(poi): Implemented bulk import / export UI for points of interest. Refs: #77

--- frontend/src/utils/file.utils.ts
+++ frontend/src/utils/file.utils.ts
@@ -1,7 +1,17 @@
 import type {CityDTO} from "../../typescript-client";
+import type {POIDTO} from "../../typescript-client";
+
 export function downloadCityArrayToUserDevice(cities: Array<CityDTO>, fileName: string = 'Cities.json') {
     const fileURL = window.URL.createObjectURL(new File([JSON.stringify(cities)], fileName));
-import type {POIDTO} from "../../typescript-client";
+    const a = document.createElement('a');
+    a.href = fileURL;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(fileURL);
+}
+
 export function downloadPOIArrayToUserDevice(pois: Array<POIDTO>, fileName: string = 'Points of Interest.json') {
     const fileURL = window.URL.createObjectURL(new File([JSON.stringify(pois)], fileName));
     const a = document.createElement('a');
