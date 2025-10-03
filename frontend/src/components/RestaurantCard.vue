e8cd09a8905405c55c5dd91ed8e5d951abf04977
Alexia_naghi <162351141+AlexiaReo@users.noreply.github.com>
2025-10-03 15:16:03 +0300

    feat(restaurant): implemented CRUD UI
    Refs:#72

---
+++ frontend/src/components/RestaurantCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+    import type {RestaurantDTO} from "../../typescript-client";
+
+    defineProps<{
+      restaurant: RestaurantDTO,
+      selected: boolean
+    }>()
+
+    const emits = defineEmits(["checkbox-clicked","card-clicked"])
+</script>
+
+<template>
+    <Card class="w-full my-8" @click="emits('card-clicked')">
+        <template #title>{{restaurant.name}}</template>
+        <template #content>
+            <div class="flex justify-between">
+                    <Checkbox :value="selected" binary @click="emits('checkbox-clicked')"/>
+            </div>
+        </template>
+    </Card>
+</template>
+
+<style scoped>
+
+</style>
