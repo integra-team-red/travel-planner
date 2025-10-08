9f47acf2e8d8d95f7c4d2f4033923446464d497e
CRLXT <corlatemanuel@gmail.com>
2025-10-08 17:43:02 +0300

    feat(users): migrated users to db

--- backend/src/main/java/cloudflight/integra/backend/user/UserServiceImpl.java
+++ backend/src/main/java/cloudflight/integra/backend/user/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package cloudflight.integra.backend.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,15 +9,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserDetailsService, UserService {
-    private final UserRepository repository;
+    private final DBUserRepository repository;
 
     @Autowired
-    public UserServiceImpl(UserRepository repository) {
+    public UserServiceImpl(DBUserRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return repository.findUserByEmail(email);
+        User user = repository.findUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(new SimpleGrantedAuthority(user.getRole().name()))
+                .build();
     }
 }
