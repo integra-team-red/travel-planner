2324a99c2de741b9bc680af59afcdd13b3239b5d
Leo Mihalcea <leomihalcea@gmail.com>
2025-10-15 17:34:07 +0300

    fix(login): remove token and redirect to login when unauthorized

--- frontend/src/interceptor/interceptor.ts
+++ frontend/src/interceptor/interceptor.ts
@@ -3,7 +3,8 @@ const originalFetch = window.fetch
 window.fetch = async (...args) => {
     const response = await originalFetch(...args)
     if (response.status === 401 || response.status === 403) {
-        //window.location.href = "/login"
+        localStorage.removeItem("jwt");
+        window.location.href = "/login"
     }
     return response
 }
