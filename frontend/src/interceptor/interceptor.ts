6c8aceef66d6a18fb5d5266ecccfc2f7adb0fbbc
Leo Mihalcea <leomihalcea@gmail.com>
2025-10-22 23:05:38 +0300

    fix(security): critical security bugs

--- frontend/src/interceptor/interceptor.ts
+++ frontend/src/interceptor/interceptor.ts
@@ -2,7 +2,7 @@ const originalFetch = window.fetch
 
 window.fetch = async (...args) => {
     const response = await originalFetch(...args)
-    if (response.status === 401 || response.status === 403 && !response.url.includes("/me")) {
+    if (response.status === 401 || response.status === 403) {
         localStorage.removeItem("jwt");
         window.location.href = "/login"
     }
